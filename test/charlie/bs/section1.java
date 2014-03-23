27dbd00b7fd22abe9ba4d5bd4289b0cb090b60e9
Devin Young <devin.young1@marist.edu>
2014-03-23 13:08:55 -0400

    updated charliePlugins

---
+++ test/charlie/bs/section1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package charlie.bs;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Devin's Work
+ */
+public class section1 {
+    
+    public section1() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void hello() {
+             System.out.println("hello");
+         assert(true);
+     }
+}
