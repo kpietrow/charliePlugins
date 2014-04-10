7e8fce1835866f96425115d1cf13437c97f495bd
Devin Young <devin.young1@marist.edu>
2014-04-10 16:43:28 -0400

    implemented and tested test shoe

--- src/charlie/sidebet/test/Shoe.java
+++ src/charlie/sidebet/test/Shoe.java
@@ -50,7 +50,75 @@ public class Shoe extends charlie.card.Shoe {
         cards.add(new Card(9,Card.Suit.SPADES));
         cards.add(new Card(8,Card.Suit.DIAMONDS)); 
         
-        cards.add(new Card(3,Card.Suit.CLUBS));        
+        cards.add(new Card(3,Card.Suit.CLUBS));
+        
+        // Test case #3
+        cards.add(new Card(9,Card.Suit.HEARTS));
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        cards.add(new Card(7,Card.Suit.SPADES));
+        cards.add(new Card(8,Card.Suit.DIAMONDS)); 
+        
+        cards.add(new Card(3,Card.Suit.CLUBS));
+        
+        // Test case #4
+        cards.add(new Card(7,Card.Suit.HEARTS));
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        cards.add(new Card(9,Card.Suit.SPADES));
+        cards.add(new Card(10,Card.Suit.DIAMONDS)); 
+        
+        cards.add(new Card(3,Card.Suit.CLUBS));
+        
+        // Test case #5
+        cards.add(new Card(9,Card.Suit.HEARTS));
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        cards.add(new Card(7,Card.Suit.SPADES));
+        cards.add(new Card(10,Card.Suit.DIAMONDS)); 
+        
+        cards.add(new Card(3,Card.Suit.CLUBS));
+        
+        // Test case #6
+        cards.add(new Card(Card.KING,Card.Suit.HEARTS));
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        cards.add(new Card(Card.QUEEN,Card.Suit.HEARTS));
+        cards.add(new Card(8,Card.Suit.DIAMONDS));
+        
+        // Test case #7
+        cards.add(new Card(Card.KING,Card.Suit.HEARTS));
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        cards.add(new Card(Card.QUEEN,Card.Suit.DIAMONDS));
+        cards.add(new Card(8,Card.Suit.DIAMONDS));
+        
+        // Test case #8
+        cards.add(new Card(8,Card.Suit.HEARTS));
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        cards.add(new Card(5,Card.Suit.SPADES));
+        cards.add(new Card(6,Card.Suit.DIAMONDS)); 
+        
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        // Test case #9
+        cards.add(new Card(7,Card.Suit.HEARTS));
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        cards.add(new Card(6,Card.Suit.SPADES));
+        cards.add(new Card(6,Card.Suit.DIAMONDS)); 
+        
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        // Test case #10
+        cards.add(new Card(6,Card.Suit.HEARTS));
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
+        
+        cards.add(new Card(7,Card.Suit.SPADES));
+        cards.add(new Card(6,Card.Suit.DIAMONDS)); 
+        
+        cards.add(new Card(Card.KING,Card.Suit.CLUBS));
     }
     
     @Override
