6ec1400839810b08ed688e90d331f9727c485fb1
Devin Young <devin.young1@marist.edu>
2014-04-10 17:46:20 -0400

    cleaned up doc comments

--- src/charlie/sidebet/rule/SideBetRule.java
+++ src/charlie/sidebet/rule/SideBetRule.java
@@ -1,26 +1,3 @@
-/*
- Copyright (c) 2014 Ron Coleman
-
- Permission is hereby granted, free of charge, to any person obtaining
- a copy of this software and associated documentation files (the
- "Software"), to deal in the Software without restriction, including
- without limitation the rights to use, copy, modify, merge, publish,
- distribute, sublicense, and/or sell copies of the Software, and to
- permit persons to whom the Software is furnished to do so, subject to
- the following conditions:
-
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package charlie.sidebet.rule;
 
 import charlie.card.Card;
@@ -31,7 +8,8 @@ import org.slf4j.LoggerFactory;
 
 /**
  * This class implements the side bet rule for Super 7.
- * @author Ron Coleman
+ * 
+ * @author Devin Young and Kevin Pietrow
  */
 public class SideBetRule implements ISideBetRule {
     private final Logger LOG = LoggerFactory.getLogger(SideBetRule.class);
@@ -46,10 +24,11 @@ public class SideBetRule implements ISideBetRule {
     /**
      * Apply rule to the hand and return the payout if the rule matches
      * and the negative bet if the rule does not match.
+     * 
      * @param hand 
      *          Hand to analyze.
      * @return 
-     *          the sidebet payout
+     *          the side bet payout
      */
     @Override
     public double apply(Hand hand) {
