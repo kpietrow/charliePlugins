2cda9f586f729f7fd4f604a3055c4ef32ab75c57
Devin Young <devin.young1@marist.edu>
2014-04-11 20:09:45 -0400

    added chip images/chip image functionality to side bet view

--- src/charlie/sidebet/view/SideBetView.java
+++ src/charlie/sidebet/view/SideBetView.java
@@ -4,6 +4,7 @@ import charlie.audio.Effect;
 import charlie.audio.SoundFactory;
 import charlie.card.Hid;
 import charlie.plugin.ISideBetView;
+import charlie.util.Constant;
 import charlie.view.AMoneyManager;
 import static charlie.view.AMoneyManager.PLACE_HOME_X;
 import static charlie.view.AMoneyManager.PLACE_HOME_Y;
@@ -11,6 +12,7 @@ import static charlie.view.AMoneyManager.STAKE_HOME_X;
 import static charlie.view.AMoneyManager.STAKE_HOME_Y;
 import charlie.view.sprite.AtStakeSprite;
 import static charlie.view.sprite.AtStakeSprite.DIAMETER;
+import charlie.view.sprite.Chip;
 
 import charlie.view.sprite.ChipButton;
 import java.awt.BasicStroke;
@@ -18,7 +20,11 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import javax.swing.ImageIcon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,6 +39,8 @@ public class SideBetView implements ISideBetView {
     public final static int X = 400;
     public final static int Y = 200;
     public final static int DIAMETER = 50;
+    protected Integer[] amounts = { 100, 25, 5 };
+    protected List<Chip> chips = new ArrayList<>();
     
     protected Font font = new Font("Arial", Font.BOLD, 18);
     protected Font descFont = new Font("Arial", Font.BOLD, 15);
@@ -48,6 +56,10 @@ public class SideBetView implements ISideBetView {
 
     protected List<ChipButton> buttons;
     protected int amt = 0;
+    private final int width;
+    protected final static String[] UP_FILES =
+        {"chip-100-1.png","chip-25-1.png","chip-5-1.png"};
+    protected Random ran = new Random();
     protected AMoneyManager moneyManager;
     protected AtStakeSprite wager = new AtStakeSprite(X-DIAMETER/2,Y-DIAMETER/2 - 5,50);
     public final static int PLACE_HOME_X = X + AtStakeSprite.DIAMETER + 10;
@@ -55,6 +67,10 @@ public class SideBetView implements ISideBetView {
 
     public SideBetView() {
         LOG.info("side bet view constructed");
+        ImageIcon icon = new ImageIcon(Constant.DIR_IMGS+UP_FILES[0]);
+
+        Image img = icon.getImage();
+        this.width = img.getWidth(null);
     }
     
     /**
@@ -81,6 +97,14 @@ public class SideBetView implements ISideBetView {
             if(button.isPressed(x, y)) {
                 amt += button.getAmt();
                 LOG.info("A. side bet amount "+button.getAmt()+" updated new amt = "+amt);
+                
+                int n = chips.size();
+                int placeX = PLACE_HOME_X + n * width/3 + ran.nextInt(10)-10;
+                
+                int placeY = PLACE_HOME_Y + ran.nextInt(5)-5;
+                
+                Chip chip = new Chip(button.getImage(),placeX - 20,placeY - 34,button.getAmt());
+                chips.add(chip);
                 SoundFactory.play(Effect.CHIPS_IN);
             } 
         }
@@ -89,6 +113,9 @@ public class SideBetView implements ISideBetView {
                 if (oldAmt == amt) {
                     amt = 0;
                     LOG.info("B. side bet amount cleared");
+                    
+                    chips.clear();
+                    
                     SoundFactory.play(Effect.CHIPS_OUT);
                 }
         }
@@ -166,6 +193,11 @@ public class SideBetView implements ISideBetView {
         int y = Y-DIAMETER/2 - 6 + DIAMETER/2 + fm.getHeight() / 4;
         
         g.drawString(amt+"", x, y);
+        
+        for(int i=0; i < chips.size(); i++) {
+            Chip chip = chips.get(i);
+            chip.render(g);
+        }
     }
 }
 
